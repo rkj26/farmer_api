986a858ce7371a80bb7ec483c42849cebce71b33
Rakshit Jha <rakshit@fifthrowtech.com>
2021-01-24 10:47:44 +0000

    random

--- farmer_api/settings.py
+++ farmer_api/settings.py
@@ -142,3 +142,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
